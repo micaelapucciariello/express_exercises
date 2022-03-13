e7ce1b2cdd297b4653ec1bb8391b0eee3fae3ba9
Micaela Pucciariello <pucciariellomicaela@gmail.com>
2022-03-12 21:16:34 -0300

    created server

---
+++ express/server.js
@@ -0,0 +1,11 @@
+const { response } = require('express');
+const express = require('express')
+const app = express()
+
+app.listen(3000, function(request,response){
+    console.log("server up!");
+});
+
+app.get("/",function(){
+    response.send("alo!");
+});
