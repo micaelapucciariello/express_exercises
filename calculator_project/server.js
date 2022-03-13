22aec998baf92e13811271a851186d450313e764
Micaela Pucciariello <pucciariellomicaela@gmail.com>
2022-03-13 17:53:58 -0300

    added body parser

--- calculator_project/server.js
+++ calculator_project/server.js
@@ -1,5 +1,8 @@
-var express = require('express')
-var app = express()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.listen(3000, function(){
     console.log("server up!");
@@ -8,4 +11,10 @@ app.listen(3000, function(){
 app.get("/",function(request, response){
     console.log(__dirname);
     response.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(request, response){
+    var result = Number(request.body.num1)+ Number(request.body.num2);
+    console.log("Result is: " + result);
+    response.send("thank you for posting!");
 });
