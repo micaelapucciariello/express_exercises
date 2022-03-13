eb8550a7d23d20f319fce4ab68d4eb20e6063240
Micaela Pucciariello <pucciariellomicaela@gmail.com>
2022-03-13 18:09:11 -0300

    added bmi calc

--- calculator_project/server.js
+++ calculator_project/server.js
@@ -13,8 +13,21 @@ app.get("/",function(request, response){
     response.sendFile(__dirname + "/index.html");
 });
 
+app.get("/bmi",function(request, response){
+    console.log(__dirname);
+    response.sendFile(__dirname + "/bmi.html");
+});
+
 app.post("/", function(request, response){
     var result = Number(request.body.num1)+ Number(request.body.num2);
+    console.log("Result is: " + result);
+    response.send("thank you for posting!");
+});
+
+app.post("/bmi", function(request, response){
+    var height = Number(request.body.height);
+    var weight = Number(request.body.mass);
+    var result = weight/(height*height);
     console.log("Result is: " + result);
     response.send("thank you for posting!");
 });
